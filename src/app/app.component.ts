368f23ae1ff3d99db5c141512314aed0136e17b4
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-02-17 15:27:56 -0300

    comments added in file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //nome que será utilizado para selecionar o componente e chama-lo em outro local da aplicação
+  selector: "app-root",
+
+  //html que é montado no componente e será exibido no browser
+  templateUrl: "./app.component.html",
+
+  //estilização que o componente utiliza
+  styleUrls: ["./app.component.css"],
 })
+
+//um componente em angular é uma classe, e a convenção para o nome da classe é PascalCase
 export class AppComponent {
-  title = 'app';
+  //aqui criamos uma propriedade chamada title, description e url, e dentro do html do componente, conseguimos acessar essas propriedades.
+  title = "alurapic";
+  description = "Leão";
+  url =
+    "https://img.elo7.com.br/product/zoom/26B13B5/painel-de-festa-dragao-2-dragao.jpg";
 }
