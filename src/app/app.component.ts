b9cee705b133a7fc9e6c8ac96d7e76ad0ba121f7
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-02-18 14:09:52 -0300

    using a constructor to create a http client , making a request to an api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 
 @Component({
@@ -16,14 +17,20 @@ export class AppComponent {
   //as propriedades são chamadas no htlm do próprio componente
   title = "alurapic";
 
-  photos = [
-    {
-      url: "https://img.elo7.com.br/product/zoom/26B13B5/painel-de-festa-dragao-2-dragao.jpg",
-      description: "",
-    },
-    {
-      url: "https://img.elo7.com.br/product/original/26B13A2/painel-de-festa-dragao-como-treinar-seu-dragao.jpg",
-      description: "",
-    },
-  ];
+  //aqui irei colocar a resposta que eu obtiver da api, utilizando o http.get
+  //dentro do retorno da api possui a propriedade url e description
+  photos: Object[] = [];
+
+  //para consumir dados de uma api com angular, eu posso fazer a configuração do httpclient de forma manual ou posso utilizar o constructor para que seja feita para mim.
+  //toda classe em ecmascript e typescript possui um constructor. dentro do parametro, eu passo http e preciso passar o tipo dele com o typescript
+  //o httpclient é o provider que mostra ao constructor como configurar o http, para que não precisemos fazer tudo de forma manual. precisamos fazer o import dentro do root module (app.module.ts)
+  constructor(http: HttpClient) {
+    //ao invés de o angular utilizar promisses ele utiliza observable. então no observable nós podemos utilizar o subscrive, que é o equivalente ao then, consigo passar uma função, passando a resposta como parâmetro e fazer o que eu quiser, como colocar a resposta dentro de uma variável
+    http
+      .get<Object[]>("http://localhost:3000/flavio/photos")
+      .subscribe((res) => {
+        console.log(res);
+        this.photos = res;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { PhotosModules } from "./photos/photos.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   //dentro de declarations, colocamos os componentes que iremos usar na aplicação.
@@ -12,7 +13,13 @@ import { PhotosModules } from "./photos/photos.module";
 
   //imports é aonde eu importo módulos.
   //importo o PhotosModules que é aonde está encapsulado os componentes que são pertinentes a um determinado conceito.
-  imports: [BrowserModule, PhotosModules],
+
+  imports: [
+    BrowserModule,
+    PhotosModules,
+    //o HttpClientModule é um módulo provider de http para fazer as configurações da aplicação, para que eu possa utilizar ele no constructor e fazer as requisições
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
